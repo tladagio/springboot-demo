4348645c4c22c22eb1358e3d0eb32d0bdd051499
wiggin <wiggin@gmail.com>
2018-05-28 23:32:44 +0800

    去掉没有用的代码

--- src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
+++ src/main/java/com/wiggin/springboot/demo/controller/StudentController.java
@@ -2,10 +2,7 @@ package com.wiggin.springboot.demo.controller;
 
 import com.wiggin.springboot.demo.model.Student;
 import com.wiggin.springboot.demo.service.StudentService;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,8 +22,6 @@ public class StudentController {
 
     @RequestMapping(value = "/qryById")
     public Student qry(int id){
-        System.out.println("this is v2");
-        System.out.println("v2");
         return service.queryById(id);
     }
 }
